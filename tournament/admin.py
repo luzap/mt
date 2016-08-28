5d76f55522b7bc05485171ce13824e179383d966
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-08-28 16:49:47 +0400

    Model editing

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import School, Individual
+from .models import School, Individual, Post, Language
 
 admin.site.register(School)
 admin.site.register(Individual)
+admin.site.register(Language)
+admin.site.register(Post)
--- tournament/models.py
+++ tournament/models.py
@@ -1,19 +1,29 @@
+import django
 from django.db import models
 from . import utils
-# Create your models here.
 
 
 # TODO Add query for number of competitors
 class School(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
-    code = models.CharField(max_length=7)
+    code = models.CharField(max_length=10)
+
+    def __init__(self):
+        self.code = utils.gen_code(self.name)
+
+    def __repr__(self):
+        return self.name
 
     class Meta:
         pass
 
 
 class Language(models.Model):
-    language = models.CharField(max_length=15)
+    id = models.AutoField(primary_key=True)
+    language = models.CharField(max_length=15, default=" ")
+
+    def __repr__(self):
+        return self.language
 
     class Meta:
         pass
@@ -24,10 +34,23 @@ class Individual(models.Model):
     surname = models.CharField(max_length=30, blank=False)
     school = models.ForeignKey(School)
     email = models.EmailField(blank=False)
-    languages = models.ForeignKey(Language)
+
+    def __repr__(self):
+        return "{} {}".format(self.name, self.surname)
 
     class Meta:
         pass
+
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    date_published = models.DateField(default=django.utils.timezone.now)
+
+    def __repr__(self):
+        return self.title
+
 
 
 
