1f7491b99fa939104fba858d5e624349574a0de4
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-08-28 18:02:06 +0400

    Admin page modifications

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import School, Individual, Post, Language
 
-admin.site.register(School)
-admin.site.register(Individual)
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'email')
+
+
+class IndividualAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email')
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_published', 'author', 'body')
+
+
+admin.site.register(School, SchoolAdmin)
+admin.site.register(Individual, IndividualAdmin)
 admin.site.register(Language)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
