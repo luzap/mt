57c65b99602cd8e8b427e5d1ce9f4722308621eb
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-08-28 15:47:42 +0400

    Working 404 page

--- tournament/urls.py
+++ tournament/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^index/', views.index, name='index'),
     url(r'^registration/', views.registration, name='registration'),
+    url(r'^404/', views.handler404, name='404')
 ]
--- tournament/views.py
+++ tournament/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, render_to_response
-from django.template import RequestContext
 
 
 def handler404(request):
-    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+    response = render_to_response('404.html', {})
     response.status_code = 404
     return response
+
 
 def index(request):
     pass
