f1c2579d74a219202febc61fb2673354346c0d89
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2016-08-28 14:43:21 +0400

    URL regex fixed

--- tournament/urls.py
+++ tournament/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
-from django.contrib import admin
 
 from . import views
 
-
-urlpatters = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^home/', views.index, name='home')
+urlpatterns = [
+    url(r'^index/', views.index, name='index'),
+    url(r'^registration/', views.registration, name='registration'),
 ]
