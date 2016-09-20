8056b7d0888478353768f77a183282099e74865b
Lukas <lukas.zapolskas@gmail.com>
2016-09-20 18:26:58 +0400

    Beginnings of auto-emailing system

--- tournament/mail.py
+++ tournament/mail.py
@@ -1,3 +1,18 @@
 from django.core import mail
+from django.template import Context
+from django.template.loader import render_to_string
 
-# Write code for email sending upon registration here
+connection = mail.get_connection()
+
+
+def send_confirmation_email(user_dict, path_to_body):
+    """Send a generic message."""
+    cont = Context(user_dict)  # Allows for passing of content to HTML template
+    html_content = render_to_string(path_to_body + ".html", cont)  # Both are needed due to the way Django handles
+    txt_content = render_to_string(path_to_body + ".txt", cont)    # html form rendering
+
+    email = mail.EmailMultiAlternatives("Registration confirmation", txt_content)
+    email.attach_alternative(html_content, 'text/html')
+    email.to = [user_dict['email']]
+    email.send()
+
